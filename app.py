51fea8a9b9fae36c65c943395c3486be4bd144ae
Meet Rajesh Gor <40317114+Mr-Destructive@users.noreply.github.com>
2023-04-10 22:58:46 +0530

    fix: move to openai again

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import cohere
+#import cohere
 import requests
 import argparse
 import subprocess
@@ -7,43 +7,41 @@ import os
 from dotenv import load_dotenv
 
 OPENAI_API_ENDPOINT = "https://api.openai.com/v1/completions"
-COHERE_API_ENDPOINT = "https://api.cohere.ai/v1/generate"
+#COHERE_API_ENDPOINT = "https://api.cohere.ai/v1/generate"
 load_dotenv()
 
 def generate_gitignore(directory_path):
     command = f"ls -a {directory_path}"
     output = subprocess.check_output(command, shell=True).decode("utf-8")
     prompt = f"Generate a .gitignore file for the following directory:\n{output}"
-    API_KEY = os.getenv("COHERE_API_KEYS")
-    print(API_KEY)
-    co = cohere.Client(API_KEY)
-    response = co.generate(
-        model='command-xlarge-nightly',  
-        prompt = prompt,  
-        max_tokens=40,  
-        temperature=0.6,  
-    )
-    response = response.generations[0].text
-
-    print(response)
-    #response = requests.post(
-    #    COHERE_API_ENDPOINT,
-    #    headers={"Authorization": f"Bearer {API_KEY}"},
-    #    json={
-    #        "prompt": prompt,
-    #        "max_tokens": 20,
-    #        "return_likelihoods": "NONE",
-    #        "truncate": "END",
-    #    },
+    API_KEY = os.getenv("OPENAI_API_KEYS")
+ 
+    #co = cohere.Client(API_KEY)
+    #response = co.generate(
+    #    model='command-xlarge-nightly',  
+    #    prompt = prompt,  
+    #    max_tokens=40,  
+    #    temperature=0.6,  
     #)
+    #response = response.generations[0].text
 
-    #if response.status_code == 200:
-    #    gitignore_text = response.json()
-    #    with open(directory_path / ".gitignore", 'w') as f:
-    #        f.write(gitignore_text)
-    #else:
-    #    print(response.content)
-    #    print("Unable to call OPEN AI API")
+    #print(response)
+    response = requests.post(
+        COHERE_API_ENDPOINT,
+        headers={"Authorization": f"Bearer {API_KEY}"},
+        json={
+            "prompt": prompt,
+            "max_tokens": 20,
+        },
+    )
+
+    if response.status_code == 200:
+        gitignore_text = response.json()
+        with open(directory_path / ".gitignore", 'w') as f:
+            f.write(gitignore_text)
+    else:
+        print(response.content)
+        print("Unable to call OPEN AI API")
 
 def main():
     parser = argparse.ArgumentParser(description='Generate a .gitignore file based on directory contents.')
